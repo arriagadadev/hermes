50ee8c892c5b4c94ead55ce65271b481ed49e2b5
Daniel Alejandro Pérez Arriagada <daniel@localhost.localdomain>
2020-01-17 11:14:55 -0300

    Table name added

--- app/UnitOfMeasurement.php
+++ app/UnitOfMeasurement.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class UnitOfMeasurement extends Model
 {
     protected $fillable = ['name', 'unit', 'variable_id'];
+    protected $table = 'units_of_measurement';
 }
