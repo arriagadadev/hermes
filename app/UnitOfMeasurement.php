c8866a2c6c9d67dd2ecd06f614532e958fe10e24
Daniel Alejandro Pérez Arriagada <daniel@localhost.localdomain>
2020-01-17 09:59:27 -0300

    Fillables attributes added

--- app/UnitOfMeasurement.php
+++ app/UnitOfMeasurement.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class UnitOfMeasurement extends Model
 {
-    //
+    protected $fillable = ['name', 'unit', 'variable_id'];
 }
--- app/Variable.php
+++ app/Variable.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Variable extends Model
 {
-    //
+    protected $fillable = ['name', 'description'];
 }
