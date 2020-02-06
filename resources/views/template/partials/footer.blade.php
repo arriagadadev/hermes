63584311b5913c654d975b76b7bc66bb93ee676a
Daniel Alejandro Pérez Arriagada <daniel@localhost.localdomain>
2020-02-06 19:33:00 +0900

    Footer changed

--- resources/views/template/partials/footer.blade.php
+++ resources/views/template/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-	<span>Copyright © 2019 Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span>
+	<span>Hermes © 2020 | All rights reserved</span>
 </footer>
