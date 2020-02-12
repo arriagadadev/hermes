f8e38d7dce91d651c6dbc790dad706fdcb230f7a
Daniel Alejandro Pérez Arriagada <daniel@localhost.localdomain>
2020-02-13 00:01:47 +0900

    device store and update functions added

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -3,8 +3,129 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Device;
 
 class DeviceController extends Controller
 {
-    //
+
+    public function getDevices(Request $request){
+    	$devices = Device::select(
+    		'devices.id',
+    		'devices.identifier',
+    		'devices.alias',
+    		'technology_types.name as technology_type',
+    		'device_types.name as device_type'
+    		)
+    	->join('technology_types','devices.technology_type_id','=','technology_types.id')
+    	->join('device_types','devices.device_type_id','=','device_types.id')
+    	->where('devices.organization_id',$request->id);
+
+        if($request->filter){
+            $devices = $devices->where(function ($query) use ($request) {
+                        $query->where('devices.alias', 'like', '%'.$request->filter.'%')
+                              ->orWhere('devices.identifier', 'like', '%'.$request->filter.'%');
+                        });
+        }
+    	$devices = $devices->paginate(5);
+
+    	return $devices;
+    }
+
+    public function getDevice(Request $request){
+        $device = Device::select(
+            'devices.id',
+            'devices.identifier',
+            'devices.alias',
+            'devices.scope',
+            'devices.active',
+            'devices.has_gps',
+            'devices.technology_type_id',
+            'devices.device_type_id',
+            'devices.latitude',
+            'devices.longitude',
+            'devices.altitude',
+            'technology_types.name as technology_type',
+            'device_types.name as device_type',
+            'devices.organization_id'
+            )
+        ->join('technology_types','devices.technology_type_id','=','technology_types.id')
+        ->join('device_types','devices.device_type_id','=','device_types.id')
+        ->where('devices.id',$request->id)->first();
+
+        return [
+            'device' => $device
+        ];
+    }
+
+    public function storeDevice(Request $request){
+        $validatedData = $request->validate($this->getValidations($request));
+        $device = new Device;
+        $device->identifier = $validatedData['device']['identifier'];
+        $device->alias = $validatedData['device']['alias'];
+        $device->scope = $validatedData['device']['scope'];
+        $device->device_type_id = $validatedData['device']['device_type_id'];
+        $device->technology_type_id = $validatedData['device']['technology_type_id'];
+        $device->active = $validatedData['device']['active'];
+        $device->has_gps = $validatedData['device']['has_gps'];
+        $device->latitude = $validatedData['device']['latitude'];
+        $device->longitude = $validatedData['device']['longitude'];
+        $device->altitude = $validatedData['device']['altitude'];
+        $device->organization_id = $validatedData['device']['organization_id'];
+        $device->save();
+        return [
+            'device' => $device
+        ];
+    }
+
+    public function updateDevice(Request $request){
+        $validatedData = $request->validate($this->getValidations($request));
+        $device = Device::findOrFail($request->device['id']);
+        $device->identifier = $request->device['identifier'];
+        $device->alias = $request->device['alias'];
+        $device->scope = $request->device['scope'];
+        $device->device_type_id = $request->device['device_type_id'];
+        $device->technology_type_id = $request->device['technology_type_id'];
+        $device->active = $request->device['active'];
+        $device->has_gps = $request->device['has_gps'];
+        $device->latitude = $request->device['latitude'];
+        $device->longitude = $request->device['longitude'];
+        $device->altitude = $request->device['altitude'];
+        $device->save();
+        return [
+            'device' => $device
+        ];
+    }
+
+    private function getValidations(Request $request){
+        $validations = [
+            'device.id' => 'integer',
+            'device.alias' => 'required',
+            'device.scope' => 'integer',
+            'device.device_type_id' => 'integer',
+            'device.technology_type_id' => 'integer',
+            'device.organization_id' => 'integer',
+            'device.has_gps' => 'boolean',
+            'device.active' => 'boolean',
+            'device.latitude' => 'nullable',
+            'device.longitude' => 'nullable',
+            'device.altitude' => 'nullable'
+        ];
+
+        //The identifier must be unique in the organization
+        $validations['device.identifier'] = Rule::unique('devices', 'identifier')
+            ->where(function ($query) use ($request){
+                return $query->where('organization_id', $request->device['organization_id']);
+            });
+
+        //If we are updating the device, we must ignore it in the validation
+        if($request->isMethod('put')){
+            if(isset($request->device['id'])){
+                $validations['device.identifier'] = $validations['device.identifier']
+                    ->ignore($request->device['id']);
+            }
+        }
+
+        return $validations;
+    }
 }
