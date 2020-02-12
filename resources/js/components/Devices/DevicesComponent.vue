29d261adf7161f7bf37599f2eba5f0c54cb7cc40
Daniel Alejandro Pérez Arriagada <daniel@localhost.localdomain>
2020-02-12 23:54:06 +0900

    Search bar added

--- resources/js/components/Devices/DevicesComponent.vue
+++ resources/js/components/Devices/DevicesComponent.vue
@@ -1,43 +1,64 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="bgc-white bd bdrs-3 p-20">
-                    <h4 class="c-grey-900">Devices</h4>
-                    <v-text-field
-                        v-model="filter"
-                        label="Filter by alias or identifier"
-                        v-on:keyup="getDevices()"
-                        clearable
-                        class="search-bar"
-                        @click:clear="clearFilter"
-                    ></v-text-field>
-                    <v-simple-table>
-                        <template v-slot:default>
-                            <thead>
-                                <tr>
-                                    <th class="text-left">Identifier</th>
-                                    <th class="text-left">Alias</th>
-                                    <th class="text-left">Type</th>
-                                    <th class="text-left">Technology</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="device in paginator.data" :key="device.id">
-                                    <td><router-link :to="'/device/'+device.id">{{ device.identifier }}</router-link></td>
-                                    <td>{{ device.alias }}</td>
-                                    <td>{{ device.device_type }}</td>
-                                    <td>{{ device.technology_type }}</td>
-                                </tr>
-                            </tbody>
-                        </template>
-                    </v-simple-table>
-                    <pagination class="paginator" :data="paginator" @pagination-change-page="getDevices"></pagination>
+    <main>
+        <v-card class="card-btn-floating">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="bgc-white bdrs-3 p-20">
+                            <h4 class="c-grey-900">Devices</h4>
+                            <v-text-field
+                                v-model="filter"
+                                label="Filter by alias or identifier"
+                                v-on:keyup="getDevices()"
+                                clearable
+                                class="search-bar"
+                                @click:clear="clearFilter"
+                            ></v-text-field>
+                            <v-simple-table>
+                                <template v-slot:default>
+                                    <thead>
+                                        <tr>
+                                            <th class="text-left">Identifier</th>
+                                            <th class="text-left">Alias</th>
+                                            <th class="text-left">Type</th>
+                                            <th class="text-left">Technology</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="device in paginator.data" :key="device.id">
+                                            <td><router-link :to="'/device/'+device.id">{{ device.identifier }}</router-link></td>
+                                            <td>{{ device.alias }}</td>
+                                            <td>{{ device.device_type }}</td>
+                                            <td>{{ device.technology_type }}</td>
+                                        </tr>
+                                    </tbody>
+                                </template>
+                            </v-simple-table>
+                            <pagination class="paginator" :data="paginator" @pagination-change-page="getDevices"></pagination>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-
-        </div>
-    </div>
+            <v-fab-transition>
+                <router-link to="/device/new-device">
+                    <v-btn
+                        absolute
+                        top
+                        right
+                        color="blue darken-2"
+                        dark
+                        outlined
+                        small
+                        fab
+                        class="table-fab"
+                    >
+                        <v-icon>add</v-icon>
+                    </v-btn>
+                </router-link>
+            </v-fab-transition>
+        </v-card>
+    </main>
 </template>
 
 <script>
@@ -45,7 +66,8 @@
         data (){
             return {
                 paginator: {},
-                filter: ''
+                filter: '',
+                fab: false
             }
         },
         methods:{
