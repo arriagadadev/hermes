6b31a92914b2fe9549c0152ebd0aab1e0060507b
Daniel Alejandro Pérez Arriagada <daniel@localhost.localdomain>
2020-02-12 23:55:43 +0900

    Validator Mixin added

---
+++ resources/js/components/Mixins/Validator.js
@@ -0,0 +1,36 @@
+export default {
+    data() {
+        return {
+            rules: {
+                required: v => !!v || 'This attribute is required.',
+                integer: v => {
+                    var re = /[^0-9]/g;
+                    if(re.exec(v ? v : '')){
+                        return "This attribute must be an integer";
+                    }
+                    return true;
+                },
+                realNumber: v => {
+                    var re = /^[-]?[0-9]+([.][0-9]{1,10})?$/;
+                    if(!re.exec(v ? v : '0')){
+                        return "The attribute must be a real number";
+                    }
+                    return true;
+                },
+                latitude: v => {
+                    if(v > 90 || v < -90){
+                        return 'This attribute must be in a range between -90 and 90';
+                    }
+                    return true;
+                },
+                longitude: v => {
+                    if(v > 180 || v < -180){
+                        return 'This attribute must be in a range between -180 and 180';
+                    }
+                    return true;
+                }
+
+            }
+        }
+    }
+};
