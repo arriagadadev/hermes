57eeafc17cd46730216fdaf8cab96835a1562d8f
Daniel Pérez <daniel.perez@swarm.cl>
2020-02-27 04:41:53 -0300

    routes are now nested

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,25 @@ Auth::routes();
 
 
 Route::group(['middleware' => ['auth']], function () {
-	Route::get('/user', 'UserController@getUserInformation');
-	Route::put('/user', 'UserController@updateUserInformation');
-	Route::put('/change-password', 'UserController@changePassword');
 
-	//Devices
-	Route::get('/organization/{organization}/devices', 'DeviceController@getDevices');
-
-	//Device
-	Route::get('/organization/{organization}/device/{device}', 'DeviceController@getDevice');
-	Route::post('/organization/{organization}/device', 'DeviceController@storeDevice');
-	Route::put('/organization/{organization}/device', 'DeviceController@updateDevice');
-
-	//Slots
-	Route::get('/organization/{organization}/device/{device}/slots', 'SlotController@getSlots');
-	Route::get('/organization/{organization}/device/{device}/slot/{slot}', 'SlotController@getSlot');
-	Route::post('/organization/{organization}/device/{device}/slot', 'SlotController@storeSlot');
-	Route::put('/organization/{organization}/device/{device}/slot', 'SlotController@updateSlot');
+	//Nested Organization routes
+	Route::prefix('/organization/{organization}')->group(function(){
+		//App
+		Route::get('/', 'AppController@index');
+		//Devices
+		Route::get('devices', 'DeviceController@getDevices');
+		Route::post('device', 'DeviceController@storeDevice');
+		Route::put('device', 'DeviceController@updateDevice');
+		//Nested Device routes
+		Route::prefix('device/{device}')->group(function(){
+			Route::get('/', 'DeviceController@getDevice');
+			//Slots
+			Route::get('slots', 'SlotController@getSlots');
+			Route::get('slot/{slot}', 'SlotController@getSlot');
+			Route::post('slot', 'SlotController@storeSlot');
+			Route::put('slot', 'SlotController@updateSlot');
+		});
+	});
 
 	//Technology types
 	Route::get('/technology-types', 'TechnologyTypeController@getTechnologyTypes');
@@ -49,9 +51,11 @@ Route::group(['middleware' => ['auth']], function () {
 	//Display modes
 	Route::get('/display-modes', 'DisplayModeController@getDisplayModes');
 
+	//User
+	Route::get('/user', 'UserController@getUserInformation');
+	Route::put('/user', 'UserController@updateUserInformation');
+	Route::put('/change-password', 'UserController@changePassword');
+
 	//Selector
 	Route::get('/', 'OrganizationSelectorController@index');
-
-	//App
-	Route::get('/organization/{organization}', 'AppController@index');
 });
